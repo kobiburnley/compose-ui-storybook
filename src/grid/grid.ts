b2e2f3baa8abb26641db786faf8bc172e3ce0dae
Kobi <qobyyyy@gmail.com>
2020-04-29 20:29:16 +0300

    order imports

--- src/grid/grid.ts
+++ src/grid/grid.ts
@@ -1,12 +1,12 @@
 import * as classNames from "classnames"
 import { domx, DomXProps } from "compose-ui-mobx-dom/es/domx"
+import { EventEmitter } from "events"
 import * as styles from "./grid.scss"
 
 export interface GridProps extends Omit<Partial<DomXProps<"div">>, "type"> {
   container?: boolean
   direction?: "row" | "column"
 }
-import {EventEmitter} from "events"
 
 export function Grid(props: GridProps) {
   return function render(_: unknown, events?: EventEmitter) {
